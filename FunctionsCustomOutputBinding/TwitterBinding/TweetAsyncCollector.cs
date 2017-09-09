69a354721d37e74c77f6454094ab478a8ff59c7b
yuka1984 <newtamafuyou@gmail.com>
2017-09-09 21:00:18 +0900

    Add Azure Functions custom output binding test code.

---
+++ FunctionsCustomOutputBinding/TwitterBinding/TweetAsyncCollector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CoreTweet;
+using Microsoft.Azure.WebJobs;
+using Newtonsoft.Json.Linq;
+
+namespace TwitterBinding
+{
+    public class TweetAsyncCollector : IAsyncCollector<TweetMessage>
+    {
+        private readonly Tokens _token;
+
+        public TweetAsyncCollector(TweetAttribute tweetAttribute)
+        {
+            _token = CoreTweet.Tokens.Create(tweetAttribute.ApiKey, tweetAttribute.ApiSeacret,
+                tweetAttribute.AccessToken, tweetAttribute.AccessSeacret);
+        }
+
+        public async Task AddAsync(TweetMessage item, CancellationToken cancellationToken = new CancellationToken())
+        {
+            await _token.Statuses.UpdateAsync(new
+            {
+                status = item.Message,
+            });
+        }
+
+        public Task FlushAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            return Task.CompletedTask;
+        }        
+    }
+}
---
+++ FunctionsCustomOutputBinding/TwitterBinding/TweetAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Description;
+
+namespace TwitterBinding
+{
+    [AttributeUsage(AttributeTargets.Parameter | AttributeTargets.ReturnValue)]
+    [Binding]
+    public class TweetAttribute : Attribute
+    {
+        [AppSetting(Default = "TwitterApiKeyName")]
+        public string ApiKey { get; set; }
+
+        [AppSetting(Default = "TwitterApiSeacretName")]
+        public string ApiSeacret { get; set; }
+
+        [AppSetting(Default = "TwitterAccessTokenName")]
+        public string AccessToken { get; set; }
+
+        [AppSetting(Default = "TwitterAccessSeacretName")]
+        public string AccessSeacret { get; set; }
+    }
+}
